9d0b57a70af90eafbd14cdb27c061bbf94432cb9
junyang <junyang@digitalgd.com.cn>
2024-01-10 15:35:45 +0800

    first commit

---
+++ dream-starter/src/main/java/cn/com/dream/common/exception/BaseException.java
@@ -0,0 +1,42 @@
+package cn.com.dream.common.exception;
+
+import cn.hutool.json.JSONUtil;
+import lombok.Data;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 自定义异常
+ *
+ * @Author yangjun
+ * @Date 2020/1/19
+ **/
+
+@Data
+public class BaseException extends RuntimeException {
+
+    /**
+     * 异常编码
+     */
+    private String code;
+
+    /**
+     * 异常信息
+     */
+    private String msg;
+
+    public BaseException(String code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String toJsonStr() {
+        Map<String, String> jsonMap = new LinkedHashMap<>(2);
+        jsonMap.put("code", code);
+        jsonMap.put("msg", msg);
+        return JSONUtil.toJsonStr(jsonMap);
+    }
+
+}
